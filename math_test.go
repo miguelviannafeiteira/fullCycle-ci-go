bea92856524b42b90b2e891599fd2cab26c418f4
Miguel Vianna <miguelviannafeiteira@gmail.com>
2023-09-18 10:35:34 -0300

    teste

--- math_test.go
+++ math_test.go
@@ -6,6 +6,6 @@ func testSoma(t *testing.T) {
 	total := soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado inválido: Resultado %d Esperado: %d", total, 30)
+		t.Errorf("Resultado inváalido: Resultado %d Esperado: %d", total, 30)
 	}
 }
